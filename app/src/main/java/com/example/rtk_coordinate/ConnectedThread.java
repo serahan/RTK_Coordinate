e97bcfb060eeab23fe7397ee42235ad275e74b07
serahan <tpfkgks44>
2021-04-14 22:07:21 +0900

    위도 경도 추출 마침

--- app/src/main/java/com/example/rtk_coordinate/ConnectedThread.java
+++ app/src/main/java/com/example/rtk_coordinate/ConnectedThread.java
@@ -4,14 +4,21 @@ import android.bluetooth.BluetoothSocket;
 import android.os.SystemClock;
 import android.util.Log;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 
 public class ConnectedThread extends Thread {
     private final BluetoothSocket mmSocket;
     private final InputStream mmInStream;
     private final OutputStream mmOutStream;
+    private InputStreamReader isReader;
+    private BufferedReader reader;
+    private StringBuffer sb = new StringBuffer();
+    String latitude = "";
+    String longitude = "";
 
     public ConnectedThread(BluetoothSocket socket) {
         mmSocket = socket;
@@ -32,19 +39,53 @@ public class ConnectedThread extends Thread {
 
     @Override
     public void run() {
-        byte[] buffer = new byte[1024];  // buffer store for the stream
+//        byte[] buffer = new byte[1024];  // buffer store for the stream
         int bytes; // bytes returned from read()
+        String str;
+        String[] splitData;
+        double up;
+        double down;
+
         // Keep listening to the InputStream until an exception occurs
         while (true) {
             try {
                 // Read from the InputStream
                 bytes = mmInStream.available();
                 if (bytes != 0) {
-                    buffer = new byte[1024];
-                    SystemClock.sleep(100); //pause and wait for rest of data. Adjust this depending on your sending speed.
-                    bytes = mmInStream.available(); // how many bytes are ready to be read?
-                    bytes = mmInStream.read(buffer, 0, bytes); // record how many bytes we actually read
-                    Log.d("TAG:readStream", "readStream : " + bytes);
+//                    buffer = new byte[1024];
+//                    SystemClock.sleep(100); //pause and wait for rest of data. Adjust this depending on your sending speed.
+//                    bytes = mmInStream.available(); // how many bytes are ready to be read?
+//                    bytes = mmInStream.read(buffer, 0, bytes); // record how many bytes we actually read
+//                    Log.d("TAG:readStream", "readStream : " + bytes);
+                    isReader = new InputStreamReader(mmInStream);
+                    reader = new BufferedReader(isReader);
+                    while((str = reader.readLine())!=null) {
+                        Log.d("TAG:readStream", "readStream : " + str + "\n");
+                        if (str.contains("GPGGA") || str.contains("GNGGA")) { // GPGGA, GNGGA를 포함하고 있는 경우
+//                            Log.d("TAG:readStream", "readStream : " + str + "\n");
+
+                            splitData = str.split(",");
+
+//                            for(int i=0; i<splitData.length;i++) {
+//                                Log.d("TAG:split", "split[" + i + "] " + splitData[i]);
+//                            }
+
+                            // 위도 계산
+                            up = Double.parseDouble(splitData[2].substring(0, 2));               // 35.00 double
+                            down = Double.parseDouble(splitData[2].substring(2));          // 56.67005 double
+                            down /= 60;
+                            latitude = Double.toString(up + down);
+
+                            // 경도 계산
+                            up = Double.parseDouble(splitData[4].substring(0,3));
+                            down = Double.parseDouble(splitData[4].substring(3));
+                            down /= 60;
+                            longitude = Double.toString(up + down);
+
+                            Log.d("TAG:test", "latitude : " + latitude);
+                            Log.d("TAG:test", "longitude : " + longitude);
+                        }
+                    }
                 }
             } catch (IOException e) {
                 e.printStackTrace();
