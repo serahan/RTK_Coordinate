0aabc9a059c3c3a8f68eb15efacd1ec093e8d054
serahan <tpfkgks44>
2021-07-05 15:42:01 +0900

    정상 작동 마침.

--- app/src/main/java/com/example/rtk_coordinate/ConnectedThread.java
+++ app/src/main/java/com/example/rtk_coordinate/ConnectedThread.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
+import android.view.ViewDebug;
 import android.widget.TextView;
 
 import java.io.BufferedInputStream;
@@ -110,28 +111,25 @@ public class ConnectedThread extends Thread {
                     Log.d("TAG:readStream", "bytes : " + bytes);
                     bytes = mmInStream.read(buffer, 0, bytes); // record how many bytes we actually read
 
-                    // 21-06-30 11:42 am
-                    // byte로 읽는 걸 mmInStream.read(buffer, 0, bytes)로 변경하여 성공했다고 생각하고 Hex 변환 진행
-
-//                    result = byteToHexString(buffer, bytes);
+                    result = byteToHexString(buffer, bytes);
                     // GNGGA
-                    result = "B56201131C0000000000F43D6C1689AF97ED7B08B61843333116F0F2200062E70000F8A624474E4747412C3038323933342E35302C333535362E36373031302C4E2C31323634302E39363338322C452C312C31322C302E37302C32312E312C4D2C31382E382C4D2C2C2A37360D0A";
+//                    result = "B56201131C0000000000F43D6C1689AF97ED7B08B61843333116F0F2200062E70000F8A624474E4747412C3038323933342E35302C333535362E36373031302C4E2C31323634302E39363338322C452C312C31322C302E37302C32312E312C4D2C31382E382C4D2C2C2A37360D0A";
                     // GNGNS
 //                    result = "B56201131C0000000000B465DA1ABAB197EDF007B618AC333116E4112400D13801003A6F24474E474E532C3035303832322E35302C333535362E36373139382C4E2C31323634302E39363133382C452C41414E412C31342C302E37322C31382E312C31382E382C2C2C562A32430D0AB5";
-                    Log.d("TAG:readStream", "result.length : " + result.length());
+//                    result = "B56201131C0000000000C4A62A06D7B197ED780AB6183A363116E115140032FE0000173424474E4747412C3034343430302E35302C333535362E36373332352C4E2C31323634302E39353836352C452C312C31322C302E38342C32362E302C4D2C31382E382C4D2C2C2A37460D";
+                    Log.d("TAG:readStream", "result : " + result);
 
                     // 전체 들어오는 대로 이어붙이기
                     strBytes = strBytes + result;
+                    Log.d("TAG:readStream", "strBytes : " + strBytes);
+
 
                     // UBX 시작부분 찾으면
                     if (strBytes.contains("B562")) {
                         B562.addAll(Arrays.asList(strBytes.split("B562")));
 
 //                        Log.d("TAG:readStream", "B562 length : " + B562.length());
-                        Log.d("TAG:readStream", "B562 length : " + B562.size());
-
-
-                        //arraylist 변경중
+                        Log.d("TAG:readStream", "B562 size() : " + B562.size());
 
                         // 뭔가 이상, 코드 이해 불가.
 //                        if (!strBytes.substring(0, 4).equals("B562")) {
@@ -148,25 +146,33 @@ public class ConnectedThread extends Thread {
 
                         // strBytes가 B5에서 끝나 split이 제대로 안되었다면 분리
                         int index = B562.get(B562.size() - 1).indexOf("0A");
-                        if (index != -1) {
-                            if (index + 1 < B562.get(B562.size() - 1).length() - 1) {
-                                B562.add(B562.get(B562.size() - 1).substring(index + 2));
-                                B562.set(B562.size() - 2, B562.get(B562.size() - 2).substring(0, index + 2));
+                        if (index > 72) {
+                            if (index != -1) {
+                                if (index + 1 < B562.get(B562.size() - 1).length() - 1) {
+                                    B562.add(B562.get(B562.size() - 1).substring(index + 2));
+                                    B562.set(B562.size() - 2, B562.get(B562.size() - 2).substring(0, index + 2));
+                                }
                             }
                         }
 
                         // 21-07-02 220 Bytes 제거
                         // arrayList의 마지막 array가 제대로 한 문장이 들어오지 않은 경우 이어서 받도록 구현
-                        if (!B562.get(B562.size() - 1).contains("0A")) {
-                            strBytes = B562.get(B562.size() - 1);
+                        // 길이가 72 초과, 마지막 두 글자가 "OA"인 경우
+                        if (!(B562.get(B562.size()-1).length() > 72) || !(B562.get(B562.size()-1).substring(B562.get(B562.size()-1).length()-2)).equals("0A")) {
+                            strBytes = B562.get(B562.size()-1);
                         } else {
                             strBytes = "";
                         }
 
+//                        if (!B562.get(B562.size() - 1).contains("0A")) {
+//                            strBytes = B562.get(B562.size() - 1);
+//                        }
+
                         // 21-07-02 220 Bytes 제거
                         // UBX + GNGNS 제대로 한문장 들어온 것 찾기
                         for (int i = B562.size() - 1; i > 0; i--) {
-                            if (B562.get(i).contains("0A")) {
+                            // if (B562.get(i).contains("0A"))
+                            if ((B562.get(B562.size()-1).length() > 72) && (B562.get(B562.size()-1).substring(B562.get(B562.size()-1).length()-2)).equals("0A")) {
                                 strHex = B562.get(i);
                                 break;
                             }
@@ -199,9 +205,15 @@ public class ConnectedThread extends Thread {
 
                             // Accuracy 추출
                             String accuracy = splitResult[0].substring(60, 68);
-                            int test = toLittleEndian(accuracy);
-                            double test_double = test * 0.0001;
-                            Accuracy = Double.toString(test_double) + "m";
+                            int accuracyInt = toLittleEndian(accuracy);
+
+                            StringBuffer accuracyString = new StringBuffer();
+                            accuracyString.append(Integer.toString(accuracyInt));
+                            accuracyString.insert(1, ".");
+                            Log.d("TAG:readStream", "StringBuffer : " + accuracyString);
+
+
+                            Accuracy = accuracyString + "m";
                             Log.d("TAG:readStream", "test : " + Accuracy);
 
                             // [STATE] splitResult[0] : UBX hex, splitResult[1] : GNGGA ASCII
